a9b23b5df6726107e1d3a6e595f6ee931b4b8cd2
Viresh Jain <vireshkj@gmail.com>
2013-07-29 22:50:17 -0500

    changes in junit test that calls the library service impl

--- SpringMVC/src/test/java/viresh/test/spring/mvc/LibraryTest.java
+++ SpringMVC/src/test/java/viresh/test/spring/mvc/LibraryTest.java
@@ -1,12 +1,15 @@
 package viresh.test.spring.mvc;
 
 import org.junit.Test;
+import org.springframework.binding.mapping.Mapper;
+import org.springframework.binding.mapping.MappingResults;
 import org.springframework.util.Assert;
 import org.springframework.web.servlet.View;
 import org.springframework.webflow.config.FlowDefinitionResource;
 import org.springframework.webflow.config.FlowDefinitionResourceFactory;
 import org.springframework.webflow.core.collection.LocalAttributeMap;
 import org.springframework.webflow.core.collection.MutableAttributeMap;
+import org.springframework.webflow.engine.EndState;
 import org.springframework.webflow.engine.Flow;
 import org.springframework.webflow.engine.VariableValueFactory;
 import org.springframework.webflow.engine.ViewState;
@@ -63,45 +66,103 @@ public class LibraryTest extends AbstractXmlFlowExecutionTests {
 
     }
 
+//    @Test
+//    public void testIntroductionFlow() {
+//        MutableAttributeMap input = new LocalAttributeMap();
+//
+//        VariableValueFactory valueFactory = mock(VariableValueFactory.class);
+//
+//        Flow flow = (Flow) getFlowDefinition();
+//        FlowExecution flowExecution = getFlowExecutionFactory().createFlowExecution(flow);
+//
+//        updateFlowExecution(flowExecution);
+//
+//
+//        getFlowExecution().start(input, context);
+//
+////        startFlow(context);
+//        assertCurrentStateEquals("welcome");
+//        assertResponseWrittenEquals("welcome", context);
+//        context.setEventId("next");
+//
+//        getFlowExecution().resume(context);
+//
+//
+//        setCurrentState("welcome");
+//        getFlowDefinitionRegistry().registerFlowDefinition(createSubflow());
+//
+//        context.setEventId("next");
+//        resumeFlow(context);
+//
+//        assertCurrentStateEquals("introduction");
+//        getFlowExecution().resume(context);
+//        assertResponseWrittenEquals("welcome", context);
+//
+////        context.setEventId("next");
+//
+////        getFlowExecution().resume(context);
+//        assertCurrentStateEquals("menu");
+////        getFlowExecution().resume(context);
+//        assertResponseWrittenEquals("welcome", context);
+//
+////        FlowResourceFlowViewResolver resolver = new FlowResourceFlowViewResolver();
+////        RequestContext context = mock(RequestContext.class);
+////
+////        LocalAttributeMap flash = new LocalAttributeMap();
+////        when(context.getFlashScope()).thenReturn(flash);
+////
+////        when(context.getCurrentState()).thenReturn(new MockViewState());
+//
+//
+////        View view = resolver.resolveView("introduction", context);
+////        assertModelAttributeNotNull("itroduction",view);
+//
+////        getFlowExecution().getActiveSession().getAttributes().getOutcome().resume(context);
+////        getFlow().getState("introduction");
+//
+//
+////        getFlowScope();
+//
+//
+//    }
+
     @Test
     public void testIntroductionFlow() {
-        MutableAttributeMap input = new LocalAttributeMap();
+        MockExternalContext context = new MockExternalContext();
 
-        VariableValueFactory valueFactory = mock(VariableValueFactory.class);
+        setCurrentState("welcome");
+        getFlowDefinitionRegistry().registerFlowDefinition(createSubflow());
 
-        Flow flow = (Flow) getFlowDefinition();
-        FlowExecution flowExecution = getFlowExecutionFactory().createFlowExecution(flow);
-
-        updateFlowExecution(flowExecution);
-
-
-        getFlowExecution().start(null, context);
-
-//        startFlow(context);
-        assertCurrentStateEquals("welcome");
         context.setEventId("next");
+        resumeFlow(context);   // this is the one that cause Library Service to be called
 
-        getFlowExecution().resume(context);
         assertCurrentStateEquals("introduction");
-        FlowResourceFlowViewResolver resolver = new FlowResourceFlowViewResolver();
-        RequestContext context = mock(RequestContext.class);
+        getFlowExecution().resume(context);
+        assertResponseWrittenEquals("introduction", context);
 
-        LocalAttributeMap flash = new LocalAttributeMap();
-        when(context.getFlashScope()).thenReturn(flash);
+//        context.setEventId("next");
 
-        when(context.getCurrentState()).thenReturn(new MockViewState());
+//        getFlowExecution().resume(context);
+        assertCurrentStateEquals("menu");
 
-
-//        View view = resolver.resolveView("introduction", context);
-//        assertModelAttributeNotNull("itroduction",view);
-
-//        getFlowExecution().getActiveSession().getAttributes().getOutcome().resume(context);
-//        getFlow().getState("introduction");
+//        getFlowExecution().resume(context);
+        assertResponseWrittenEquals("introduction", context);
 
 
-        getFlowScope();
+
+    }
 
 
+    private Flow createSubflow() {
+        Flow mockBookingFlow = new Flow("flows/introduction");
+        mockBookingFlow.setInputMapper(new Mapper() {
+            public MappingResults map(Object source, Object target) {
+// assert that 1L was passed in as input
+                return null;
+            }
+        });
+        new EndState(mockBookingFlow, "menu");
+        return mockBookingFlow;
     }
 
     @Test
